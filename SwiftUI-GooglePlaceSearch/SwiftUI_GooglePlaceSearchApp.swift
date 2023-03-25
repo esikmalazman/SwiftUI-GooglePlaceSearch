7ffb1410f6182590e00e615a3f8a095d6752ea3a
Ikmal Azman <opikmalazman@gmail.com>
2023-03-25 05:44:13 +0000

    Initial Commit

---
+++ SwiftUI-GooglePlaceSearch/SwiftUI_GooglePlaceSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GooglePlaceSearchApp.swift
+//  SwiftUI-GooglePlaceSearch
+//
+//  Created by Ikmal Azman on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GooglePlaceSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
